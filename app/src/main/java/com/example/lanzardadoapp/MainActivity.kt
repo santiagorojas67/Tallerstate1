31416f04c2675075751b7ec25993545bfe6b1b90
Santiago Rojas <santiagortarazona@gmail.com>
2026-09-24 16:56:23 -0500

    feat:Agrega estado y visualizacion del dado

--- app/src/main/java/com/example/lanzardadoapp/MainActivity.kt
+++ app/src/main/java/com/example/lanzardadoapp/MainActivity.kt
@@ -4,13 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
@@ -18,9 +18,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.lanzardadoapp.ui.theme.LanzardadoappTheme
 
 class MainActivity : ComponentActivity() {
@@ -30,7 +29,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             LanzardadoappTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    CounterScreen(modifier = Modifier.padding(innerPadding))
+                    DiceScreen(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -38,25 +37,29 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun CounterScreen(modifier: Modifier = Modifier) {
+fun DiceScreen(modifier: Modifier = Modifier) {
 
-    var contador by remember { mutableIntStateOf(0) }
+    val diceImages = listOf(
+        R.drawable.dice_1,
+        R.drawable.dice_2,
+        R.drawable.dice_3,
+        R.drawable.dice_4,
+        R.drawable.dice_5,
+        R.drawable.dice_6
+    )
+
+
+    var currentDice by remember { mutableIntStateOf(diceImages[0]) }
 
     Column(
         modifier = modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        Text(
-            text = "$contador",
-            fontSize = 64.sp,
-            fontWeight = FontWeight.Bold
+        Image(
+            painter = painterResource(id = currentDice),
+            contentDescription = "Dado",
+            modifier = Modifier.size(200.dp)
         )
-
-        Button(onClick = {
-            contador += 1
-        }) {
-            Text(text = "Sumar")
-        }
     }
 }
